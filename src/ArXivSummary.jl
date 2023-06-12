9cb4d8a2aa0f579934d46e40a3ed7069b0bec349
ryubee1980 <ryubee@gmail.com>
2023-06-12 12:31:15 +0900

    first commint

--- src/ArXivSummary.jl
+++ src/ArXivSummary.jl
@@ -1,5 +1,29 @@
 module ArXivSummary
 
+
+using OpenAI
 # Write your package code here.
+
+function getGPT(;KEY="OPENAI_API_KEY.txt")
+
+    fn=open(KEY)
+        
+    line=readline(fn)
+
+    
+    secret_key=line
+    model = "gpt-3.5-turbo"
+    #prompt =  "Say \"this is a test\""
+    prompt =  "iphoneとは何ですか？"
+
+    r = create_chat(
+        secret_key, 
+        model,
+        [Dict("role" => "user", "content"=> prompt)]
+    )
+    println(r.response[:choices][begin][:message][:content])
+    #println(r)
+end
+
 
 end
